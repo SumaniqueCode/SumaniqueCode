9a7bbcbdaa5639679f36c82b6406a9883accde46
Suman Regmi <limitlessplaying@gmail.com>
2025-03-03 13:08:30 +0545

    Updated dependencies and configurations

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Ensure this matches your actual content files
+  ],
+  darkMode: "class", // Enables dark mode based on a class
+  theme: {
+    extend: {
+      screens: {
+        'xs': '320px', // Custom 'xs' breakpoint for mobile
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
