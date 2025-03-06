3a77d7f6c1e29b3d574d1711b13d35b2648fb2ad
Suman Regmi <limitlessplaying@gmail.com>
2025-03-06 11:54:45 +0545

    Added landing page and elements as index

--- src/portfolio/landing/Landing.tsx
+++ src/portfolio/landing/Landing.tsx
@@ -1,10 +1,5 @@
 import { useThemeContext } from "../../ThemeContext"
-import About from "./components/about/About"
-import Contact from "./components/contact/Contact"
-import Experience from "./components/experience/Experience"
 import Hero from "./components/Hero"
-import Projects from "./components/projects/Projects"
-import Skills from "./components/skills/Skills"
 
 // Main App Component
 const Landing = () => {
@@ -13,21 +8,6 @@ const Landing = () => {
         <main className="container w-9/10 mx-auto px-4 pb-12">
             {/* Hero Section */}
             <Hero darkMode={darkMode} setActiveSection={setActiveSection} />
-
-            {/* About Section */}
-            <About darkMode={darkMode} />
-
-            {/* Skills Section */}
-            <Skills darkMode={darkMode} />
-
-            {/* Projects Section */}
-            <Projects darkMode={darkMode} />
-
-            {/* Experience Section */}
-            <Experience darkMode={darkMode} />
-
-            {/* Contact Section */}
-            <Contact darkMode={darkMode} />
         </main>
     )
 }
