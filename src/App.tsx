4f87b86da7ad9abb293dc37e6c1b1f4e5e5d7a11
Suman Regmi <limitlessplaying@gmail.com>
2025-03-06 11:51:03 +0545

    Added landing page and elements as index

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './portfolio/layout/Layout'
 import '../index.css'
-
+import Landing from './portfolio/landing/Landing'
 
 function App() {
 
   return (
-      <Router>
-        <Routes>
-          <Route path='/' element={<Layout />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Layout />} >
+          <Route index element={<Landing />} />
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/portfolio/landing/Landing.tsx
@@ -0,0 +1,34 @@
+import { useThemeContext } from "../../ThemeContext"
+import About from "./components/about/About"
+import Contact from "./components/contact/Contact"
+import Experience from "./components/experience/Experience"
+import Hero from "./components/Hero"
+import Projects from "./components/projects/Projects"
+import Skills from "./components/skills/Skills"
+
+// Main App Component
+const Landing = () => {
+    const { darkMode, setActiveSection } = useThemeContext()
+    return (
+        <main className="container w-9/10 mx-auto px-4 pb-12">
+            {/* Hero Section */}
+            <Hero darkMode={darkMode} setActiveSection={setActiveSection} />
+
+            {/* About Section */}
+            <About darkMode={darkMode} />
+
+            {/* Skills Section */}
+            <Skills darkMode={darkMode} />
+
+            {/* Projects Section */}
+            <Projects darkMode={darkMode} />
+
+            {/* Experience Section */}
+            <Experience darkMode={darkMode} />
+
+            {/* Contact Section */}
+            <Contact darkMode={darkMode} />
+        </main>
+    )
+}
+export default Landing
