e093f6bc542972a726e8761eff3085af513a8a86
Suman Regmi <limitlessplaying@gmail.com>
2025-03-07 09:47:34 +0545

    Updated about section in landing

--- src/portfolio/landing/Landing.tsx
+++ src/portfolio/landing/Landing.tsx
@@ -1,4 +1,5 @@
 import { useThemeContext } from "../../ThemeContext"
+import About from "./components/about/About"
 import Hero from "./components/Hero"
 
 // Main App Component
@@ -8,6 +9,9 @@ const Landing = () => {
         <main className="container w-9/10 mx-auto px-4 pb-12">
             {/* Hero Section */}
             <Hero darkMode={darkMode} setActiveSection={setActiveSection} />
+
+            {/*About Section */}
+            <About darkMode={darkMode} />
         </main>
     )
 }
