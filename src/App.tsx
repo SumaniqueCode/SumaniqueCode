72f0652a7a2dfb8672a555161e21c53eba7ba85f
Suman Regmi <limitlessplaying@gmail.com>
2025-03-28 15:48:15 +0545

    Minor fix

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout />} >
           <Route index element={<Landing />} />
-          <Route path='commands' element={<Commands />} />
+          <Route path='/commands' element={<Commands />} />
         </Route>
       </Routes>
     </Router>
