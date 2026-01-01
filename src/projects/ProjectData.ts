e6a9098de1a056ef2638919b314f33f370b27a68
Suman Regmi <limitlessplaying@gmail.com>
2026-01-01 20:30:27 +0545

    restructured folder

---
+++ src/projects/ProjectData.ts
@@ -0,0 +1,62 @@
+export const projects = [
+  {
+    id: "1",
+    name: "Share Insight Nepal",
+    description:
+      "A Nepal stock market management web application designed to help investors manage their share portfolio efficiently by tracking holdings, profits, and risks.",
+    image: "/images/projects/shareinsightnepal/landingpage.jpg",
+    techs: ["Laravel", "PHP", "Tailwind CSS", "MySQL"],
+    codelink: "https://github.com/SumaniqueCode/shareInsightNepal",
+    livelink: "https://shareinsightnepal.up.railway.app",
+  },
+  {
+    id: "2",
+    name: "Daily Repo",
+    description:
+      "A collaborative daily task and reporting system with real-time updates and clean UI, built for teams to track progress and reports efficiently.",
+    image: "/images/projects/dailyrepo/dashboard.jpg",
+    techs: ["React", "TypeScript", "Redux", "Axios", "Tailwind CSS", "Laravel"],
+    codelink: "https://github.com/SumaniqueCode/Daily-report-react-laravel",
+    livelink: "#",
+  },
+  {
+    id: "3",
+    name: "Skill Karma",
+    description:
+      "A time-based skill exchange platform where users can offer, exchange, or monetize skills. Includes map-based skill discovery and Kanban-style task tracking.",
+    image: "/images/projects/skillkarma/dashboard.jpg",
+    techs: ["Flutter", "Laravel", "REST API", "MySQL", "Google Maps API"],
+    codelink: "https://github.com/SumaniqueCode/skill-karma",
+    livelink: "#",
+  },
+  {
+    id: "4",
+    name: "MemeNote",
+    description:
+      "A Flutter-based note-taking app that allows users to save notes with memes and images, combining productivity with creativity.",
+    image: "/images/projects/memenote/home.jpg",
+    techs: ["Flutter", "Dart", "SQLite"],
+    codelink: "https://github.com/SumaniqueCode/MemeNote",
+    livelink: "#",
+  },
+  {
+    id: "5",
+    name: "Portfolio Website",
+    description:
+      "A personal developer portfolio showcasing projects, skills, animations, and professional experience with a modern interactive UI.",
+    image: "/images/projects/portfolio/home.jpg",
+    techs: ["React", "Tailwind CSS", "Framer Motion"],
+    codelink: "https://github.com/SumaniqueCode/portfolio",
+    livelink: "https://sumanr.com.np",
+  },
+  {
+    id: "6",
+    name: "Job Portal System",
+    description:
+      "A full-featured job portal with recruiter and candidate roles, company management, job posting, filtering, and application tracking.",
+    image: "/images/projects/jobportal/dashboard.jpg",
+    techs: ["Django", "Python", "PostgreSQL", "Tailwind CSS"],
+    codelink: "https://github.com/SumaniqueCode/job-portal-django",
+    livelink: "#",
+  },
+];
