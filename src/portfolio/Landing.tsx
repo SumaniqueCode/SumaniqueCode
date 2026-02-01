0b22c0fc80cd477f08c9ac89122638eeba68811e
Suman Regmi <limitlessplaying@gmail.com>
2026-02-01 09:25:27 +0545

    Minor fix

--- src/portfolio/Landing.tsx
+++ src/portfolio/Landing.tsx
@@ -1,39 +1,30 @@
-import { useEffect } from "react"
-import { useThemeContext } from "../ThemeContext"
-import About from "./components/about/About"
-import Contact from "./components/contact/Contact"
-import Experience from "./components/experience/Experience"
-import Hero from "./components/Hero"
-import Projects from "./components/projects/Projects"
-import Skills from "./components/skills/Skills"
+import { useEffect } from "react";
+import { useThemeContext } from "../ThemeContext";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Experience from "./components/experience/Experience";
+import Hero from "./components/Hero";
+import Projects from "./components/projects/Projects";
+import Skills from "./components/skills/Skills";
 
-// Main App Component
 const Landing = () => {
-    const sideNavs = ['about','skills','projects','experience','contact']
-    const { darkMode, setActiveSection,setSideNavs } = useThemeContext()
-    useEffect(()=>(
-        setSideNavs(sideNavs)
-    ),[]);
-    return (
-        <main className="container w-9/10 mx-auto px-4 pb-12">
-            {/* Hero Section */}
-            < Hero darkMode={darkMode} setActiveSection={setActiveSection} />
+  const sideNavs = ["about", "skills", "projects", "experience", "contact"];
+  const { darkMode, setActiveSection, setSideNavs } = useThemeContext();
 
-            {/*About Section */}
-            <About darkMode={darkMode} />
+  useEffect(() => {
+    setSideNavs(sideNavs);
+  }, []);
 
-            {/*Skills Section */}
-            <Skills darkMode={darkMode} />
+  return (
+    <main className="container w-9/10 mx-auto px-4 pb-12">
+      <Hero darkMode={darkMode} setActiveSection={setActiveSection} />
+        <About darkMode={darkMode} />
+        <Skills darkMode={darkMode} />
+        <Projects darkMode={darkMode} />
+        <Experience darkMode={darkMode} />
+        <Contact darkMode={darkMode} />
+    </main>
+  );
+};
 
-            {/*Projects Section */}
-            <Projects darkMode={darkMode} />
-
-            {/*Experience Section */}
-            <Experience darkMode={darkMode} />
-
-            {/*Contact Section */}
-            <Contact darkMode={darkMode} />
-        </main>
-    )
-}
-export default Landing
+export default Landing;
