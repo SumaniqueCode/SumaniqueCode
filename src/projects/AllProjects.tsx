20b5266196c2b242860ce8c815d9855b4565f63c
SumaniqueCode <github.regmisuman@gmail.com>
2026-03-08 12:00:37 +0545

    Lazy section optimized

--- src/projects/AllProjects.tsx
+++ src/projects/AllProjects.tsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { useThemeContext } from "../ThemeContext";
 import ProjectCard from "./Components/ProjectCard"
 import { projects } from "./ProjectData"
-import LazySection from "../global/LazySection";
 
 const AllProjects = () => {
   const sideNavs = [''];
@@ -11,17 +10,16 @@ const AllProjects = () => {
     setSideNavs(sideNavs)
   ), []); return (
     <section className={'min-h-screen w-9/10 mx-auto py-6 px-4'} >
-      <LazySection>
-        <h2 className={`text-3xl font-bold mb-6 text-center ${darkMode ? 'text-blue-400' : 'text-blue-600'}`}>
-          All Projects
-        </h2>
+      <h2 className={`text-3xl font-bold mb-6 text-center ${darkMode ? 'text-blue-400' : 'text-blue-600'}`}>
+        All Projects
+      </h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6" style={{ animation: "fadeInDown 0.6s ease-out" }}>
-          {projects.map((project) => (
-            <ProjectCard key={project.id} darkMode={darkMode} project={project} />
-          ))}
-        </div>
-      </LazySection>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6" style={{ animation: "fadeInDown 0.6s ease-out" }}>
+
+        {projects.map((project) => (
+          <ProjectCard key={project.id} darkMode={darkMode} project={project} />
+        ))}
+      </div>
     </section>
   )
 }
