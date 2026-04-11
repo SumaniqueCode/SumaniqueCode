80fb9cc1ea9b6ee51c81598efd7b995e92989e78
SumaniqueCode <github.regmisuman@gmail.com>
2026-04-11 19:46:58 +0545

    optimized assets

---
+++ src/assets/images/index/hero/index.ts
@@ -0,0 +1,18 @@
+export { default as hero_Section1Original } from "./Hero_Section_1.png";
+
+export const optimizedHero_Section1 = {
+  avif: [
+    "/src/assets/images/index/hero/optimized/hero_section_1-480.avif 480w",
+    "/src/assets/images/index/hero/optimized/hero_section_1-768.avif 768w",
+    "/src/assets/images/index/hero/optimized/hero_section_1-1200.avif 1200w",
+    "/src/assets/images/index/hero/optimized/hero_section_1-1600.avif 1600w"
+  ].join(", "),
+  webp: [
+    "/src/assets/images/index/hero/optimized/hero_section_1-480.webp 480w",
+    "/src/assets/images/index/hero/optimized/hero_section_1-768.webp 768w",
+    "/src/assets/images/index/hero/optimized/hero_section_1-1200.webp 1200w",
+    "/src/assets/images/index/hero/optimized/hero_section_1-1600.webp 1600w"
+  ].join(", "),
+  fallback: "/src/assets/images/index/hero/optimized/hero_section_1-1600.jpg",
+};
+
